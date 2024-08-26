e9c0a9b194beaa7bb7dc1ed64ba92878722034c5
Lucas <lucasdamata01@hotmail.com>
2024-08-26 11:45:03 -0300

    Correção no local de salvamento

--- operacoes/coletar_dados.py
+++ operacoes/coletar_dados.py
@@ -5,7 +5,7 @@ timezone_offset = -3.0  # Pacific Standard Time (UTC−03:00)
 tzinfo = timezone(timedelta(hours=timezone_offset))
 data = datetime.now(tzinfo)
 data = f"{data.day}_{data.month}_{data.year}"
-file = f'.\dados\{data}.csv'
+file = f'..\dados\{data}.csv'
 
 os.chdir("./clima")
 os.system(f"scrapy crawl clima -o {file} -t csv")
