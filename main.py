62ca4281b4cb81c6fbc71b7b80d3353f997739a8
LucasMGuima <lucasdamata01@hotmail.com>
2024-09-03 16:00:16 -0300

    Inicio plot

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import dearpygui.dearpygui as dpg
+import os, threading, w_tabela, fichario as f
 
 from ref import Tag, Label
-
-import os, threading, w_tabela, fichario as f
+from datetime import date
 
 def _run_coleta():
     os.system("python .\operacoes\coletar_dados.py")
@@ -23,9 +23,6 @@ def callback_coletarDados():
 global fichario
 fichario = f.Fichario( pd.read_csv('./dados_tratados/dados.csv', index_col=0))
 
-global lst_openWindos
-lst_openWindos = []
-
 dpg.create_context()
 
 #Janela principal
@@ -36,6 +33,40 @@ with dpg.window(tag=Tag.WindowPrimary):
             dpg.add_menu_item(label=Label.MenuItemTabela, callback=callback_showTabel)
         with dpg.menu(label=Label.MenuOperacoes):
             dpg.add_menu_item(label=Label.MenuItemColetar, callback=callback_coletarDados)
+    
+#Janela de plot
+with dpg.window(label="Plot Test", height=400, width=800):
+    fichario.filtrar_cidade('São Paulo')
+    dias = fichario.get_columnEntries('Dia', agrupar=False)
+    tempMax = fichario.get_columnEntries('Temp Max', agrupar=False)
+
+    print(dias)
+    print(tempMax)
+
+    x_axis = []
+    for i in dias:
+        data = i.split('/')
+        dia = int(data[0])
+        mes = int(data[1])
+        temp = mes + (0.1 * dia)
+        x_axis.append(temp)
+
+    y_axis = []
+    for i in tempMax:
+        y_axis.append(int(i))
+
+    dados = fichario.get_dados()
+
+    with dpg.plot(label="Line Series", height=400, width=400):
+        # optionally create legend
+        dpg.add_plot_legend()
+
+        # REQUIRED: create x and y axes
+        dpg.add_plot_axis(dpg.mvXAxis, label="Dia")
+        dpg.add_plot_axis(dpg.mvYAxis, label="Temp. Max", tag="y_axis")
+
+        # series belong to a y axis
+        dpg.add_line_series(x_axis, y_axis, label="Temperatura Máxima", parent="y_axis")
 
 dpg.create_viewport(title='PI6SEM', x_pos=0, y_pos=0)
 dpg.set_primary_window(Tag.WindowPrimary, True)
