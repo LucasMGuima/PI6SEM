a7552c7668fb8ba69dcc744db4e3149583f745ea
Lucas <lucasdamata01@hotmail.com>
2024-09-04 10:05:49 -0300

    Valores dinâmicos

--- main.py
+++ main.py
@@ -19,6 +19,72 @@ def callback_coletarDados():
     thread = threading.Thread(target=_run_coleta)
     thread.start()
 
+def mudar():
+    global fichario
+
+    #Painel de plot
+    fichario.filtrar_cidade('Rio de Janeiro')
+    dias = fichario.get_columnEntries('Dia', agrupar=False)
+    tempMax = fichario.get_columnEntries('Temp Max', agrupar=False)
+    tempMin = fichario.get_columnEntries('Temp Min', agrupar=False)
+    #Remove o filtro do fichario 
+    fichario.limpar_filtros()
+
+    x_axis = []
+
+    for i in range(len(dias)):
+        x_axis.append(float(i))
+    # Cria as labels do eixo x
+    x_label = []
+    for i in x_axis:
+        x_label.append((str(dias[int(i)]).replace('/2024', ''), i))
+    x_label = tuple(x_label)
+
+    dpg.configure_item("window plot", label="Temperatura do Rio de Janeiro")
+    dpg.set_value('yAxis Temp. Max', [x_axis, tempMax])
+    dpg.set_value('yAxis Temp. Min', [x_axis, tempMin])
+
+def painel_temperatura():
+    global fichario
+
+    #Painel de plot
+    fichario.filtrar_cidade('São Paulo')
+    dias = fichario.get_columnEntries('Dia', agrupar=False)
+    tempMax = fichario.get_columnEntries('Temp Max', agrupar=False)
+    tempMin = fichario.get_columnEntries('Temp Min', agrupar=False)
+    #Remove o filtro do fichario 
+    fichario.limpar_filtros()
+
+    x_axis, y_axis = [], []
+
+    #Junta as duas temperaturas e remove repetições
+    temperatura = list(set(tempMax + tempMin))
+    temperatura.sort()
+
+    for i in range(len(dias)):
+        x_axis.append(float(i))
+    # Cria as labels do eixo x
+    x_label = []
+    for i in x_axis:
+        x_label.append((str(dias[int(i)]).replace('/2024', ''), i))
+    x_label = tuple(x_label)
+
+    for i in temperatura:
+        y_axis.append(float(i))
+
+    with dpg.plot(tag='window plot', label="Temperatura de São Paulo", height=400, width=800):
+        # optionally create legend
+        dpg.add_plot_legend()
+
+        # REQUIRED: create x and y axes
+        dpg.add_plot_axis(dpg.mvXAxis, label="Data (Dia/Mês)")
+        dpg.set_axis_ticks(dpg.last_item(),x_label)
+        dpg.add_plot_axis(dpg.mvYAxis, label="Temperatura em °C", tag="y_axis")
+
+        # Cria a linha de Temperatura máxima
+        dpg.add_line_series(x_axis, tempMax, label="Temp. Máxima", parent="y_axis", tag="yAxis Temp. Max")
+        # Cria a linha de Temperatura minima
+        dpg.add_line_series(x_axis, tempMin, label="Temp. Mínima", parent="y_axis", tag="yAxis Temp. Min")
 
 global fichario
 fichario = f.Fichario( pd.read_csv('./dados_tratados/dados.csv', index_col=0))
@@ -34,37 +100,10 @@ with dpg.window(tag=Tag.WindowPrimary):
         with dpg.menu(label=Label.MenuOperacoes):
             dpg.add_menu_item(label=Label.MenuItemColetar, callback=callback_coletarDados)
     
-    #Painel de plot
-    fichario.filtrar_cidade('São Paulo')
-    dias = fichario.get_columnEntries('Dia', agrupar=False)
-    tempMax = fichario.get_columnEntries('Temp Max', agrupar=False)
-
-    x_axis, y_axis = [], []
-
-    for i in range(len(dias)):
-        x_axis.append(float(i))
-    for i in tempMax:
-        y_axis.append(float(i))
-
-    dados = fichario.get_dados()
-
-    with dpg.plot(label="Temperatura", height=400, width=800):
-        # optionally create legend
-        dpg.add_plot_legend()
-
-        # REQUIRED: create x and y axes
-        # Cria as labels do eixo x
-        x_label = []
-        for i in x_axis:
-            x_label.append((dias[int(i)], i))
-        x_label = tuple(x_label)
-        print(x_label)
-        dpg.add_plot_axis(dpg.mvXAxis, label="Dia")
-        dpg.set_axis_ticks(dpg.last_item(),x_label)
-        dpg.add_plot_axis(dpg.mvYAxis, label="Temp. Max", tag="y_axis")
-
-        # Cria a linha de Temperatura máxima
-        dpg.add_line_series(x_axis, y_axis, label="Temperatura Máxima", parent="y_axis")
+    painel_temperatura()
+    dpg.add_button(label="Mudar", callback=mudar)
+    
+    
 
 dpg.create_viewport(title='PI6SEM', x_pos=0, y_pos=0)
 dpg.set_primary_window(Tag.WindowPrimary, True)
