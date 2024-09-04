67acfb60bd6cee5cfcccee5d8d5e2d0cb4de897f
Lucas <lucasdamata01@hotmail.com>
2024-09-04 11:16:02 -0300

    Filtro por estado e limpeza

--- w_temperatura.py
+++ w_temperatura.py
@@ -3,10 +3,20 @@ import my_window
 import dearpygui.dearpygui as dpg
 
 class WindowTemperatura(my_window.myWindow):
-    def __init__(self, fichario: f.Fichario, cidade: str = "São Paulo") -> None:
+    def __init__(self, fichario: f.Fichario, estado:str = "SP", cidade: str = "São Paulo") -> None:
         self.id = id(self)
         self.fichario = fichario
         self.cidade = cidade
+        self.estado = estado
+
+        #Tags
+        self.tagWPlot = f"wPlot {self.id}"
+        self.tagYAxis = f"yAxis {self.id}"
+        self.tagYTempMin = f"yTempMin {self.id}"
+        self.tagYTempMax = f"yTempMax {self.id}"
+        self.tagInpCidade = f"inpCidade {self.id}"
+        self.tagInpEstado = f"inpEstado {self.id}"
+        self.tagInpPeriodo = f"inpPeriodo {self.id}"
 
     def criar_janela(self) -> None:
         """
@@ -39,33 +49,37 @@ class WindowTemperatura(my_window.myWindow):
             y_axis.append(float(i))
         with dpg.window(label="Gráfico de Temperatura",width=850,height=450):
             with dpg.group(horizontal=True):
-                with dpg.plot(tag=f'window plot-{self.id}', label=f"Temperatura de(o) {self.cidade}", height=400, width=600):
+                with dpg.plot(tag=self.tagWPlot, label=f"Temperatura de(o) {self.cidade},{self.estado}", height=400, width=600):
                     # optionally create legend
                     dpg.add_plot_legend()
 
                     # REQUIRED: create x and y axes
                     dpg.add_plot_axis(dpg.mvXAxis, label="Data (Dia/Mês)")
                     dpg.set_axis_ticks(dpg.last_item(),x_label)
-                    dpg.add_plot_axis(dpg.mvYAxis, label="Temperatura em °C", tag=f"y_axis-{self.id}")
+                    dpg.add_plot_axis(dpg.mvYAxis, label="Temperatura em °C", tag=self.tagYAxis)
 
                     # Cria a linha de Temperatura máxima
-                    dpg.add_line_series(x_axis, tempMax, label="Temp. Máxima", parent=f"y_axis-{self.id}", tag=f"yAxis Temp. Max-{self.id}")
+                    dpg.add_line_series(x_axis, tempMax, label="Temp. Máxima", parent=self.tagYAxis, tag=self.tagYTempMax)
                     # Cria a linha de Temperatura minima
-                    dpg.add_line_series(x_axis, tempMin, label="Temp. Mínima", parent=f"y_axis-{self.id}", tag=f"yAxis Temp. Min-{self.id}")
+                    dpg.add_line_series(x_axis, tempMin, label="Temp. Mínima", parent=self.tagYAxis, tag=self.tagYTempMin)
                 
                 with dpg.group():
                     with dpg.group(horizontal=True):
+                        dpg.add_text('Estado: ')
+                        dpg.add_input_text(tag=self.tagInpEstado, width=100)
+                    dpg.add_separator()
+                    with dpg.group(horizontal=True):
                         dpg.add_text('Cidade: ')
-                        dpg.add_input_text(tag=f"Cidade-{self.id}", width=100)
-                        dpg.add_button(label="Mudar", callback=self._atualizar_painel)
-                    #with dpg.group(horizontal=True):
-                    #    dpg.add_text('Estado: ')
-                    #    dpg.add_input_text(tag="Estado")
+                        dpg.add_input_text(tag=self.tagInpCidade, width=100)
+                    dpg.add_separator()
+                    dpg.add_button(label="Mostrar", callback=self._atualizar_painel)
 
     def _atualizar_painel(self) -> None:
         #Atualiza os dados no mainel
-        self.cidade = dpg.get_value(f"Cidade-{self.id}")
+        self.cidade = dpg.get_value(self.tagInpCidade)
+        self.estado = dpg.get_value(self.tagInpEstado)
         #Painel de plot
+        if not self.fichario.filtrar_estado(self.estado): return
         if not self.fichario.filtrar_cidade(self.cidade): return
         dias = self.fichario.get_columnEntries('Dia', agrupar=False)
         tempMax = self.fichario.get_columnEntries('Temp Max', agrupar=False)
@@ -83,6 +97,6 @@ class WindowTemperatura(my_window.myWindow):
             x_label.append((str(dias[int(i)]).replace('/2024', ''), i))
         x_label = tuple(x_label)
 
-        dpg.configure_item(f"window plot-{self.id}", label=f"Temperatura de(o) {self.cidade}")
-        dpg.set_value(f'yAxis Temp. Max-{self.id}', [x_axis, tempMax])
-        dpg.set_value(f'yAxis Temp. Min-{self.id}', [x_axis, tempMin])
+        dpg.configure_item(self.tagWPlot, label=f"Temperatura de(o) {self.cidade}, {self.estado}")
+        dpg.set_value(self.tagYTempMax, [x_axis, tempMax])
+        dpg.set_value(self.tagYTempMin, [x_axis, tempMin])
