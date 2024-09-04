06fd7eee34e2ae155b4f8bf4d06c8cbb1cf5a15b
Lucas <lucasdamata01@hotmail.com>
2024-09-04 11:25:48 -0300

    Dia 04/09/2024

--- clima/clima/spiders/climaScrapper.py
+++ clima/clima/spiders/climaScrapper.py
@@ -8,7 +8,7 @@ class ClimaScrapper(scrapy.Spider):
     start_urls = ['https://g1.globo.com/previsao-do-tempo/indice/']
 
     def parse(self, response):
-        for i in range(65,91):
+        for i in range(65,91):#A até Z
             letter = str.lower(chr(i))
             cidades = response.css(f'div.desktop > section#index-{letter} > ul > li')
 
@@ -18,11 +18,14 @@ class ClimaScrapper(scrapy.Spider):
                 yield response.follow(link, self.parse_cidade)
 
     def parse_cidade(self, response):
-        yield {
-            'Cidade': response.css('div > div > p.forecast-header__place::text').get().split(',')[0],
-            'Estado': response.css('div > div > p.forecast-header__place::text').get().split(',')[1],
-            'Temp Max': response.css('div > div.forecast-today > div:nth-child(4) > div.forecast-today__temperature--max::text').get(),
-            'Temp Min': response.css('div > div.forecast-today > div:nth-child(4) > div.forecast-today__temperature--min::text').get(),
-            'Umidade': response.css('div > div.forecast-today-detail > div > div:nth-child(2) > div:nth-child(4) > span:nth-child(3)::text').get(),
-            'Umidade Min': response.css('div > div.forecast-today-detail > div > div:nth-child(2) > div:nth-child(4) > span:nth-child(4)::text').get()
-        }
+        try:
+            yield {
+                'Cidade': response.css('div > div > p.forecast-header__place::text').get().split(',')[0],
+                'Estado': response.css('div > div > p.forecast-header__place::text').get().split(',')[1],
+                'Temp Max': response.css('div > div.forecast-today > div:nth-child(4) > div.forecast-today__temperature--max::text').get(),
+                'Temp Min': response.css('div > div.forecast-today > div:nth-child(4) > div.forecast-today__temperature--min::text').get(),
+                'Umidade': response.css('div > div.forecast-today-detail > div > div:nth-child(2) > div:nth-child(4) > span:nth-child(3)::text').get(),
+                'Umidade Min': response.css('div > div.forecast-today-detail > div > div:nth-child(2) > div:nth-child(4) > span:nth-child(4)::text').get()
+            }
+        except:
+            pass
