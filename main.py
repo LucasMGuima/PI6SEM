1eb088c5d4a86bc7b169e3901c32a7b5f6e36c82
Lucas <lucasdamata01@hotmail.com>
2024-09-09 11:28:43 -0300

    Protótipo tela inicial

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import os, threading, w_tabela, w_temperatura, fichario as f
 from ref import Tag, Label
 from datetime import date
 
+# Funcoes internas
 def _run_coleta():
     os.system("python .\operacoes\coletar_dados.py")
     os.system("python .\operacoes\\tratar_dados.py")
     global fichario
     fichario = f.Fichario(pd.read_csv('./dados_tratados/dados.csv', index_col=0))
 
+# Callbacks
 def callback_showTabel():
     win_tabela = w_tabela.WindowTabela(fichario)
     win_tabela.criar_janela()
@@ -23,8 +25,29 @@ def callback_showTemperatura():
     p_temp = w_temperatura.WindowTemperatura(fichario)
     p_temp.criar_janela()
 
+# Funcoes de paines
+def painel_configPesquisa():
+    with dpg.child_window(width=250):
+        dpg.add_text("Dados de pesquisa")
+        with dpg.group():
+            with dpg.group(horizontal=True):
+                dpg.add_text("Estado: ")
+                dpg.add_combo()
+            with dpg.group(horizontal=True):
+                dpg.add_text("Cidade: ")
+                dpg.add_combo()
+            dpg.add_separator()
+            dpg.add_text("Periodo")
+            with dpg.group(horizontal=True):
+                dpg.add_text("De: ")
+                dpg.add_combo()
+            with dpg.group(horizontal=True):
+                dpg.add_text("Até: ")
+                dpg.add_combo()
+            dpg.add_button(label="Mostrar")
+
 global fichario
-fichario = f.Fichario( pd.read_csv('./dados_tratados/dados.csv', index_col=0))
+fichario = f.Fichario(pd.read_csv('./dados_tratados/dados.csv', index_col=0))
 
 dpg.create_context()
 
@@ -37,13 +60,25 @@ with dpg.window(tag=Tag.WindowPrimary):
             dpg.add_menu_item(label="Gráfico de Temperatura", callback=callback_showTemperatura)
         with dpg.menu(label=Label.MenuOperacoes):
             dpg.add_menu_item(label=Label.MenuItemColetar, callback=callback_coletarDados)
+    
+    with dpg.group(horizontal=True):
+        painel_configPesquisa()
+
+        with dpg.group():
+            with dpg.child_window(height=200):
+                with dpg.group(horizontal=True):
+                    dpg.add_text("Score: ")
+                    dpg.add_text("BOM")
+                dpg.add_plot()
+        
+            with dpg.child_window(height=300):
+                dpg.add_plot()
 
     
-    
 
-dpg.create_viewport(title='PI6SEM', x_pos=0, y_pos=0)
+dpg.create_viewport(title='PI6SEM', width=1000, height=600, x_pos=0, y_pos=0)
 dpg.set_primary_window(Tag.WindowPrimary, True)
 dpg.setup_dearpygui()
-dpg.show_viewport(maximized=True)
+dpg.show_viewport()
 dpg.start_dearpygui()
 dpg.destroy_context()
