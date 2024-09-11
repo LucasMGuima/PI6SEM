dfe1652cb3fc53eec3e88fe8116d3536b8b3cfbe
Lucas <lucasdamata01@hotmail.com>
2024-09-11 11:32:31 -0300

    Dia 11/09/2024

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import pandas as pd
 import dearpygui.dearpygui as dpg
 import os, threading, w_tabela, w_temperatura, fichario as f
 
-from ref import Tag, Label
+from ref import Tag, Label, UR
 from datetime import date
+
+global fichario
+global pieExist
+
+pieExist = False
+fichario = f.Fichario(pd.read_csv('./dados_tratados/dados.csv', index_col=0))
 
 # Funcoes internas
 def _run_coleta():
@@ -25,6 +31,38 @@ def _get_cidadePorEstado() -> list[str]:
         fichario.limpar_filtros()
         dpg.configure_item(Tag.ComboCidade, items=cidades)
 
+def _creat_pieChart() -> None:
+    global pieExist
+    if(pieExist): 
+        dpg.delete_item("pie")
+    else: pieExist = True
+    
+    with dpg.plot(label="Total de dias em relação a Umidade Relativa do Ar", height=250, tag="pie", parent="slot_2"):
+        x_axis = fichario.get_columnEntries("Qualidade UR")
+        y_axis = fichario.get_columnEntries("Qualidade UR", agrupar=False)
+
+        values = []
+        for x in x_axis:
+            count = 0
+            for y in y_axis:
+                if x == y: count += 1
+            percent = (count*100)/(len(y_axis))
+            values.append(percent)
+        
+        labels = []
+        for x in x_axis:
+            labels.append(UR.qualidade[x])
+        
+        # create legend
+        dpg.add_plot_legend()
+
+        dpg.add_plot_axis(dpg.mvXAxis, label="", no_gridlines=True, no_tick_marks=True, no_tick_labels=True)
+        dpg.set_axis_limits(dpg.last_item(), 0, 1)
+
+        with dpg.plot_axis(dpg.mvYAxis, label="", no_gridlines=True, no_tick_marks=True, no_tick_labels=True):
+            dpg.set_axis_limits(dpg.last_item(), 0, 1)
+            dpg.add_pie_series(0.5, 0.5, 0.25, values, labels, tag=Tag.piePlot)
+
 def _carregarDados() -> None:
     """
         Carrega os dados da cidade/estado especificado na tela
@@ -41,8 +79,10 @@ def _carregarDados() -> None:
         dias = fichario.get_columnEntries('Dia', agrupar=False)
         tempMax = fichario.get_columnEntries('Temp Max', agrupar=False)
         tempMin = fichario.get_columnEntries('Temp Min', agrupar=False)
-        fichario.limpar_filtros()
 
+        _creat_pieChart()
+
+        fichario.limpar_filtros()
         x_axis = []
 
         for i in range(len(dias)):
@@ -85,11 +125,7 @@ def painel_configPesquisa():
             #    dpg.add_combo()
             dpg.add_button(label="Mostrar", callback=_carregarDados)
 
-global fichario
-fichario = f.Fichario(pd.read_csv('./dados_tratados/dados.csv', index_col=0))
-
 dpg.create_context()
-
 #Janela principal
 with dpg.window(tag=Tag.WindowPrimary):
     #Cria a toolbar/menubar
@@ -99,17 +135,14 @@ with dpg.window(tag=Tag.WindowPrimary):
             dpg.add_menu_item(label="Gráfico de Temperatura", callback=callback_showTemperatura)
         with dpg.menu(label=Label.MenuOperacoes):
             dpg.add_menu_item(label=Label.MenuItemColetar, callback=callback_coletarDados)
-    
+        
     with dpg.group(horizontal=True):
         painel_configPesquisa()
 
         with dpg.group():
             with dpg.group(horizontal=True):
-                with dpg.child_window(height=270, width=500):
-                    with dpg.group(horizontal=True):
-                        dpg.add_text("Score: ")
-                        dpg.add_text("BOM")
-                    dpg.add_plot()
+                with dpg.child_window(height=270, width=500, tag="slot_2"):
+                    _creat_pieChart()
                 with dpg.child_window(height=270, width=200):
                     dpg.add_text("Medias/Amplitude")
         
@@ -145,7 +178,6 @@ with dpg.window(tag=Tag.WindowPrimary):
                     dpg.add_line_series(x_axis, tempMax, label="Temp. Máxima", parent=Tag.plotYAxis, tag=Tag.plotYTempMax)
                     # Cria a linha de Temperatura minima
                     dpg.add_line_series(x_axis, tempMin, label="Temp. Mínima", parent=Tag.plotYAxis, tag=Tag.plotYTempMin)
-    
 
 dpg.create_viewport(title='PI6SEM', width=1000, height=700, x_pos=0, y_pos=0)
 dpg.set_primary_window(Tag.WindowPrimary, True)
